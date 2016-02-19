5bc8e6bb0b3494c0d0078b885fd3006f68c83681
Roy Hewitt <roy_hewitt@fws.gov>
2016-02-19 15:02:08 -0500

    Set up karma to run tests in three browsers automatically

--- karma.conf.js
+++ karma.conf.js
@@ -10,8 +10,11 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'browserify'],
 
+    browserify: {
+      debug: true
+    },
 
     // list of files / patterns to load in the browser
     files: [
@@ -27,13 +30,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/test/**/*.js': 'browserify'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
 
     // web server port
